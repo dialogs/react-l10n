6936c385d9fb65369dec01fd2ce713a9baec80b2
Nikita Gusakov <gusnkt@gmail.com>
2017-01-23 16:32:13 +0300

    fix: Provider prop types

--- src/types.js
+++ src/types.js
@@ -33,9 +33,7 @@ export const ProviderPropType = {
   messages: PropTypes.objectOf(
     PropTypes.objectOf(PropTypes.string)
   ).isRequired,
-  globalValues: PropTypes.objectOf(
-    PropTypes.objectOf(PropTypes.string)
-  ).isRequired,
+  globalValues: PropTypes.objectOf(PropTypes.string).isRequired,
   children: PropTypes.element.isRequired
 };
 
